08dd79141e31e6267b8f814f6b42d1c593c1ac86
tproykov <tproykov@gmail.com>
2024-07-28 22:17:02 +0100

    second commit today

--- src/PF01ConditionalStatementsAndLoops/E10PadawanEquipment.java
+++ src/PF01ConditionalStatementsAndLoops/E10PadawanEquipment.java
@@ -1,4 +1,16 @@
 package PF01ConditionalStatementsAndLoops;
 
+import java.util.Scanner;
+
 public class E10PadawanEquipment {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
+
 }
