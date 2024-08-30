347e82c0c3521e0b3d68fef14fd46b7867b41258
tproykov <tproykov@gmail.com>
2024-08-30 17:01:51 +0100

    exercise12

--- src/PF02DataTypesAndVariables/L012RefactorSpecialNumbers.java
+++ src/PF02DataTypesAndVariables/L012RefactorSpecialNumbers.java
@@ -1,4 +1,15 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class L012RefactorSpecialNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
