48ff3bf561bbaabb8f3b2c5973c2e4ea1f318d4d
tproykov <tproykov@gmail.com>
2024-08-31 22:08:44 +0100

    exercise 8

--- src/PF02DataTypesAndVariables/L08LowerOrUpper.java
+++ src/PF02DataTypesAndVariables/L08LowerOrUpper.java
@@ -1,4 +1,21 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class L08LowerOrUpper {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        char character = scanner.nextLine().charAt(0);
+
+        if (character >= 'A' && character <= 'Z') {
+            System.out.println("upper-case");
+        }
+        else if (character >= 'a' && character <= 'z') {
+            System.out.println("lower-case");
+        }
+    }
 }
+// Write a program that prints whether a given character is upper-case or lower-case.
