b31e4e0dd74859b1e68bdf2feff76891f0b5b545
tproykov <tproykov@gmail.com>
2024-08-31 12:02:47 +0100

    exercise 11

--- src/PF01ConditionalStatementsAndLoops/L11EvenNumber.java
+++ src/PF01ConditionalStatementsAndLoops/L11EvenNumber.java
@@ -8,9 +8,16 @@ public class L11EvenNumber {
 
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.parseInt(scanner.nextLine());
 
-
-
+        while (number % 2 != 0) {
+            System.out.println("Please write an even number.");
+            number = Integer.parseInt(scanner.nextLine());
+        }
+        System.out.println("The number is: " + Math.abs(number));
     }
-
 }
+
+// Take as an input an even number and print its absolute value with a message:
+// "The number is: {absoluteValue}". If the number is odd, print "Please write an
+// even number." and continue reading numbers.
