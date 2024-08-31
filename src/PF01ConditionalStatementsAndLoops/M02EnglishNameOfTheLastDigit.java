10d0bcd74f6f209e679093832545e6b2694984a7
tproykov <tproykov@gmail.com>
2024-08-31 19:13:48 +0100

    exercise 2

--- src/PF01ConditionalStatementsAndLoops/M02EnglishNameOfTheLastDigit.java
+++ src/PF01ConditionalStatementsAndLoops/M02EnglishNameOfTheLastDigit.java
@@ -8,7 +8,44 @@ public class M02EnglishNameOfTheLastDigit {
 
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.parseInt(scanner.nextLine());
 
+        int digit = number % 10;
 
+        switch (digit) {
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+        }
     }
 }
+
+// Write a method that returns the English name of the last digit of a given number.
+// Write a program that reads an integer and prints the returned value from this method.
