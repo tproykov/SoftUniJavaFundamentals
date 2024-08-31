d88d92b053b183c18a276d72947d5e2b92ef38fe
tproykov <tproykov@gmail.com>
2024-08-31 21:39:14 +0100

    exercise 5

--- src/PF02DataTypesAndVariables/L05ConcatNames.java
+++ src/PF02DataTypesAndVariables/L05ConcatNames.java
@@ -1,11 +1,18 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class L05ConcatNames {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
 
+        System.out.println(firstName.concat(delimiter).concat(lastName));
     }
-
 }
+// Read two names and a delimiter. Print the names joined by the delimiter.
