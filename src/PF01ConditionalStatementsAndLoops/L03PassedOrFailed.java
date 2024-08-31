654b63c4a2df36b07be73100a309e3edb3a075d9
tproykov <tproykov@gmail.com>
2024-08-31 10:37:03 +0100

    two more exercises

--- src/PF01ConditionalStatementsAndLoops/L03PassedOrFailed.java
+++ src/PF01ConditionalStatementsAndLoops/L03PassedOrFailed.java
@@ -8,9 +8,18 @@ public class L03PassedOrFailed {
 
         Scanner scanner = new Scanner(System.in);
 
+        double grade = Double.parseDouble(scanner.nextLine());
 
-        
-
+        if (grade >= 3.00) {
+            System.out.println("Passed!");
+        }
+        else {
+            System.out.println("Failed!");
+        }
     }
-
 }
+
+// Write a program that:
+//•	Read a floating-point number, which represents a grade
+//•	If the grade is equal or more than 3.00, print "Passed!"
+//•	If the grade is smaller than 3.00, print "Failed!"
