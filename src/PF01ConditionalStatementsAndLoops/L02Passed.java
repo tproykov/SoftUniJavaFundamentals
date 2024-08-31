9e27cfc7e9da6ca029d600681bda66a3f3336a65
tproykov <tproykov@gmail.com>
2024-08-31 10:28:25 +0100

    exercise 2

--- src/PF01ConditionalStatementsAndLoops/L02Passed.java
+++ src/PF01ConditionalStatementsAndLoops/L02Passed.java
@@ -1,4 +1,22 @@
 package PF01ConditionalStatementsAndLoops;
 
+import java.util.Scanner;
+
 public class L02Passed {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        if (grade >= 3.00) {
+
+            System.out.println("Passed!");
+        }
+    }
 }
+
+// Write a program that:
+//•	Read a floating-point number, which represents a grade
+//•	If the grade is equal or more than 3.00, print "Passed!"
