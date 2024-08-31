8f681dffbc479696d1e31981fd6e9ccec0a6407c
tproykov <tproykov@gmail.com>
2024-08-31 11:08:40 +0100

    more pushes

--- src/PF01ConditionalStatementsAndLoops/L06ForeignLanguages.java
+++ src/PF01ConditionalStatementsAndLoops/L06ForeignLanguages.java
@@ -8,10 +8,27 @@ public class L06ForeignLanguages {
 
         Scanner scanner = new Scanner(System.in);
 
+        String country = scanner.nextLine();
 
-
-
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+        }
     }
-
-
 }
+
+// Write a program that:
+//•	Reads a string from the console, representing country
+//•	Print:
+//o	"English" -> if the entered country is "USA" or "England"
+//o	"Spanish" -> if the entered country is "Spain" or "Argentina" or "Mexico"
+//o	"unknown" -> if the entered country is any other different from countries listed above
