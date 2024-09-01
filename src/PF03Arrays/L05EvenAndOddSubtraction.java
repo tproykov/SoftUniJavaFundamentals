0e7c23f8d2c42a64c3f7bc962b3a2acab6733380
tproykov <tproykov@gmail.com>
2024-09-01 19:56:47 +0100

    even odd sum exercise

--- src/PF03Arrays/L05EvenAndOddSubtraction.java
+++ src/PF03Arrays/L05EvenAndOddSubtraction.java
@@ -1,4 +1,31 @@
 package PF03Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L05EvenAndOddSubtraction {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt)
+                .toArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] % 2 == 0) {
+                evenSum += numbers[i];
+            }
+            else {
+                oddSum += numbers[i];
+            }
+        }
+        System.out.println(evenSum - oddSum);
+    }
 }
+
+// Write a program that calculates the difference between the sum of the even and the sum of the odd
+// numbers in an array.
