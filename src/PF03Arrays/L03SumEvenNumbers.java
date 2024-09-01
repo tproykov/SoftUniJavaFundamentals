d0ad913affaa838da7c168c0e4e349d87bf0a566
tproykov <tproykov@gmail.com>
2024-09-01 19:33:25 +0100

    sum even numbers

--- src/PF03Arrays/L03SumEvenNumbers.java
+++ src/PF03Arrays/L03SumEvenNumbers.java
@@ -1,4 +1,26 @@
 package PF03Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L03SumEvenNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
+    }
 }
+
+// Read an array from the console and sum only the even numbers.
