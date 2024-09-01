2ca1c72abcf83017fe7fb101a67087d665cbf1b1
Todor Proykov <tproykov@gmail.com>
2024-09-01 07:16:26 +0100

    exercise 10

--- src/PF02DataTypesAndVariables/L10SpecialNumbers.java
+++ src/PF02DataTypesAndVariables/L10SpecialNumbers.java
@@ -1,4 +1,33 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class L10SpecialNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+
+            int sum = 0;
+            int number = i;
+            while (number != 0) {
+                sum += number % 10;
+                number = number / 10;
+            }
+            if (sum == 5 || sum == 7 || sum == 11) {
+                System.out.println(i + " -> True");
+            }
+            else {
+                System.out.println(i + " -> False");
+            }
+        }
+    }
 }
+
+// A number is special when its sum of digits is 5, 7, or 11.
+//Write a program to read an integer n and for all numbers in the range 1…n to print the number
+// and if it is special or not (True / False).
