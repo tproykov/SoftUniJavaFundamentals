0879c7dba11f52a2d38160897a72314f70d59740
tproykov <tproykov@gmail.com>
2024-09-01 19:28:21 +0100

    print numbers in reverse order

--- src/PF03Arrays/L02PrintNumbersInReverseOrder.java
+++ src/PF03Arrays/L02PrintNumbersInReverseOrder.java
@@ -1,4 +1,23 @@
 package PF03Arrays;
 
+import java.util.Scanner;
+
 public class L02PrintNumbersInReverseOrder {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] numbers = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            numbers[i] = Integer.parseInt(scanner.nextLine());
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
 }
+
+// Read n numbers and print them in reverse order.
