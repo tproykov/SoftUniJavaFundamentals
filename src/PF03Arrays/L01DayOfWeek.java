fbc589828d75e1837a184b70ce0304a1bd961ed3
tproykov <tproykov@gmail.com>
2024-09-01 19:06:08 +0100

    lab 1 arrays

--- src/PF03Arrays/L01DayOfWeek.java
+++ src/PF03Arrays/L01DayOfWeek.java
@@ -1,4 +1,24 @@
 package PF03Arrays;
 
+import java.util.Scanner;
+
 public class L01DayOfWeek {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        if (n < 1 || n > 7) {
+            System.out.println("Invalid day!");
+        }
+        else {
+            System.out.println(weekDays[n-1]);
+        }
+    }
 }
+
+// Enter a day number and print the day name (in English) or "Invalid day!". Use an array of strings.
