23a83dff9cbe4926287944e0c2458816df8da128
Todor Proykov <tproykov@gmail.com>
2024-09-05 16:16:46 +0300

    methods

--- src/PF04Methods/L01SignOfIntegerNumbers.java
+++ src/PF04Methods/L01SignOfIntegerNumbers.java
@@ -1,4 +1,25 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class L01SignOfIntegerNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(signOfInteger(n));
+    }
+    private static void signOfInteger(int n) {
+        if (n > 0) {
+            System.out.println("The number " + n + " is positive.");
+        } else if (n < 0) {
+            System.out.println("The number " + n + " is negative.");
+        }
+        else {
+            System.out.println("The number " + n + " is zero.");
+        }
+    }
 }
