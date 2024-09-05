5ff78d39768e9f6d619a2ef64767370ab9c4c630
Todor Proykov <tproykov@gmail.com>
2024-09-05 17:55:10 +0300

    rectangle area

--- src/PF04Methods/L06CalculateRectangleArea.java
+++ src/PF04Methods/L06CalculateRectangleArea.java
@@ -1,4 +1,22 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class L06CalculateRectangleArea {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double width = Double.parseDouble(scanner.nextLine());
+        double length = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf("%.0f", rectangleArea(width, length));
+    }
+    private static double rectangleArea(double width, double length) {
+        return width * length;
+    }
 }
+
+// Create a method that calculates and returns the area of a rectangle by
+// given width and length.
