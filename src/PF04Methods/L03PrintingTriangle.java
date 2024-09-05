0d931505569c3e76c24ebdd7b2357b2e86983cee
Todor Proykov <tproykov@gmail.com>
2024-09-05 17:17:25 +0300

    triangle

--- src/PF04Methods/L03PrintingTriangle.java
+++ src/PF04Methods/L03PrintingTriangle.java
@@ -1,4 +1,48 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class L03PrintingTriangle {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        printTriangle(n);
+    }
+    private static void printTriangle(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+        for (int row = n - 1; row > 0; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+// Create a method for printing triangles as shown below:
+//Examples
+//Input	Output
+//3
+//1
+//1 2
+//1 2 3
+//1 2
+//1
+
+//4
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3
+//1 2
+//1
