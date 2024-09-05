6f8f9fba14052ff314bf135068165729b9381a2d
Todor Proykov <tproykov@gmail.com>
2024-09-05 18:09:35 +0300

    power exercise

--- src/PF04Methods/L08MathPower.java
+++ src/PF04Methods/L08MathPower.java
@@ -1,4 +1,22 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class L08MathPower {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double number = Double.parseDouble(scanner.nextLine());
+        double power = Double.parseDouble(scanner.nextLine());
+
+        System.out.printf("%.0f", numberPower(number, power));
+    }
+    private static double numberPower(double number, double power) {
+        return Math.pow(number, power);
+    }
 }
+
+// Create a method that calculates and returns the value of a number raised
+// to a given power.
