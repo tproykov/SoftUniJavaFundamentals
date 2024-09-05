e8e00d8c470165924f22fff9b30657d1257e7775
Todor Proykov <tproykov@gmail.com>
2024-09-05 17:06:11 +0300

    grades

--- src/PF04Methods/L02Grades.java
+++ src/PF04Methods/L02Grades.java
@@ -30,3 +30,10 @@ public class L02Grades {
         return "Not valid";
     }
 }
+
+// Write a method that receives a grade between 2.00 and 6.00 and prints the corresponding grade in words:
+//•	2.00 – 2.99 - "Fail"
+//•	3.00 – 3.49 - "Poor"
+//•	3.50 – 4.49 - "Good"
+//•	4.50 – 5.49 - "Very good"
+//•	5.50 – 6.00 - "Excellent"
