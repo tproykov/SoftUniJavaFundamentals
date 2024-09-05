ef4a7cf9d429692e508981b98fd32b57546573cc
Todor Proykov <tproykov@gmail.com>
2024-09-05 20:10:15 +0300

    evenbyodds

--- src/PF04Methods/L10MultiplyEvensByOdds.java
+++ src/PF04Methods/L10MultiplyEvensByOdds.java
@@ -1,4 +1,33 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class L10MultiplyEvensByOdds {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Math.abs(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println(getMultipliedEvensOdds(n));
+    }
+    private static int getMultipliedEvensOdds(int n) {
+        int evenSum = 0;
+        int oddSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit % 2 == 0) {
+                evenSum += digit;
+            }
+            else {
+                oddSum += digit;
+            }
+            n = n / 10;
+        }
+        return evenSum * oddSum;
+    }
 }
+
+// Create a program that reads an integer number and multiplies the sum of
+// all its even digits by the sum of all its odd digits:
