12c4496b443b5bf2767d5b8ad8562bec8f1983f5
Todor Proykov <tproykov@gmail.com>
2024-09-06 03:03:38 +0300

    tribonacci

--- src/PF04Methods/M04TribonacciSequence.java
+++ src/PF04Methods/M04TribonacciSequence.java
@@ -1,4 +1,39 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class M04TribonacciSequence {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        getTribonacci(n);
+    }
+    private static void getTribonacci(int n) {
+
+        long num1 = 1;
+        long num2 = 1;
+        long num3 = 2;
+
+        long tribonacci = 0;
+        for (int i = 1; i <= n; i++) {
+
+            tribonacci = num1;
+            long num4 = num3 + num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            num3 = num4;
+            System.out.print(tribonacci + " ");
+        }
+    }
 }
+
+// In the "Tribonacci" sequence, every number is formed by the sum of the
+// previous 3.
+//You are given a number num. Write a program that prints num numbers from
+// the Tribonacci sequence, each on a new line, starting from 1. The input
+// comes as a parameter named num. The value num will always be a positive
+// integer.
