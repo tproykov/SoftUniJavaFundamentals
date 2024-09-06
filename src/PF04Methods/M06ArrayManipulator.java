8a63b54b0c6ce312eacc19b87ee656ffc50a7088
Todor Proykov <tproykov@gmail.com>
2024-09-06 08:43:43 +0300

    Update M06ArrayManipulator.java

--- src/PF04Methods/M06ArrayManipulator.java
+++ src/PF04Methods/M06ArrayManipulator.java
@@ -1,4 +1,16 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class M06ArrayManipulator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        
+
+    }
+
 }
