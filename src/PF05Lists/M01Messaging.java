2edc25dd71626d7777b2a645cc3c3b069ec3a5a2
Todor Proykov <tproykov@gmail.com>
2024-09-10 21:55:48 +0300

    Update M01Messaging.java

--- src/PF05Lists/M01Messaging.java
+++ src/PF05Lists/M01Messaging.java
@@ -1,4 +1,15 @@
 package PF05Lists;
 
+import java.util.Scanner;
+
 public class M01Messaging {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
