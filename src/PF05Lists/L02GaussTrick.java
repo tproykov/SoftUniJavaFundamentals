13d2f010770b252abc94f1f7bebca8c1d959886c
Todor Proykov <tproykov@gmail.com>
2024-09-10 20:08:54 +0300

    gauss trick

--- src/PF05Lists/L02GaussTrick.java
+++ src/PF05Lists/L02GaussTrick.java
@@ -1,4 +1,38 @@
 package PF05Lists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class L02GaussTrick {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> list = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        List<Integer> result = new ArrayList<>();
+        if (list.size() % 2 == 0) {
+            for (int i = 0; i < list.size() / 2; i++) {
+                result.add(list.get(i) + list.get(list.size() - 1 - i));
+            }
+        }
+        else {
+            for (int i = 0; i < list.size() / 2; i++) {
+                result.add(list.get(i) + list.get(list.size() - 1 - i));
+            }
+            result.add(list.get(list.size() /2));
+        }
+        for (Integer integer : result) {
+            System.out.print(integer + " ");
+        }
+    }
 }
+
+// Write a program that sum all numbers in a list in the following order:
+//first + last, first + 1 + last - 1, first + 2 + last - 2, … first + n,
+// last - n.
