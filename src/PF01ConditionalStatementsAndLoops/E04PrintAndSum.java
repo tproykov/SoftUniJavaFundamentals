c09717233b8990537ac8e0a544a36e2c19b82a20
Todor Proykov <tproykov@gmail.com>
2024-09-11 19:57:07 +0300

    print and sum

--- src/PF01ConditionalStatementsAndLoops/E04PrintAndSum.java
+++ src/PF01ConditionalStatementsAndLoops/E04PrintAndSum.java
@@ -1,4 +1,26 @@
 package PF01ConditionalStatementsAndLoops;
 
+import java.util.Scanner;
+
 public class E04PrintAndSum {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int startNumber = Integer.parseInt(scanner.nextLine());
+        int endNumber = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = startNumber; i <= endNumber; i++) {
+            sum += i;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Sum: " + sum);
+    }
 }
+
+// Write a program to display numbers from given start to given end and their sum.
+// All the numbers will be integers. On the first line, you will receive the start
+// number, on the second the end number.
