e20841b33389b7868b7beac5504908913d6f0b22
Todor Proykov <tproykov@gmail.com>
2024-09-11 21:39:32 +0300

    triangle

--- src/PF01ConditionalStatementsAndLoops/E08TriangleOfNumbers.java
+++ src/PF01ConditionalStatementsAndLoops/E08TriangleOfNumbers.java
@@ -8,7 +8,18 @@ public class E08TriangleOfNumbers {
 
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        for (int rows = 1; rows <= n; rows++) {
+
+            for (int cols = 1; cols <= rows; cols++) {
+
+                System.out.print(rows + " ");
+            }
+            System.out.println();
+        }
     }
-
 }
+
+// Write a program that receives a number – n and prints a triangle from 1 to n as
+// in the examples.
