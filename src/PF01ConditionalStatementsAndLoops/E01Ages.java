e280c08821936d74d10e036ba8a1e50415da4f4c
Todor Proykov <tproykov@gmail.com>
2024-09-11 19:30:24 +0300

    exercises

--- src/PF01ConditionalStatementsAndLoops/E01Ages.java
+++ src/PF01ConditionalStatementsAndLoops/E01Ages.java
@@ -1,4 +1,39 @@
 package PF01ConditionalStatementsAndLoops;
 
+import java.util.Scanner;
+
 public class E01Ages {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(determineAge(age));
+    }
+    private static String determineAge(int age) {
+        if (age >= 0 && age <= 2) {
+            return "baby";
+        }
+        else if (age >= 3 && age <= 13) {
+            return "child";
+        }
+        else if (age >= 14 && age <= 19) {
+            return "teenager";
+        }
+        else if (age >= 20 && age <= 65) {
+            return "adult";
+        }
+        return "elder";
+    }
 }
+
+// Write a program that determines whether a person is based on the given age:
+// baby, child, teenager, adult, or elder. The bounders are:
+//•	0-2 – baby;
+//•	3-13 – child;
+//•	14-19 – teenager;
+//•	20-65 – adult;
+//•	>=66 – elder;
+//•	All the values are inclusive.
--- src/PF01ConditionalStatementsAndLoops/E02Division.java
+++ src/PF01ConditionalStatementsAndLoops/E02Division.java
@@ -1,4 +1,41 @@
 package PF01ConditionalStatementsAndLoops;
 
+import java.util.Scanner;
+
 public class E02Division {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(maxDivisor(n));
+    }
+    private static String maxDivisor(int n) {
+        if (n % 10 == 0) {
+            return "The number is divisible by 10";
+        }
+        else if (n % 7 == 0) {
+            return "The number is divisible by 7";
+        }
+        else if (n % 6 == 0) {
+            return "The number is divisible by 6";
+        }
+        else if (n % 3 == 0) {
+            return "The number is divisible by 3";
+        }
+        else if (n % 2 == 0) {
+            return "The number is divisible by 2";
+        }
+        return "Not divisible";
+    }
 }
+
+// You will be given an integer, and you have to print on the console whether that
+// number is divisible by the following numbers: 2, 3, 6, 7, 10. You should always
+// take the bigger division. If the number is divisible by both 2 and 3 it is also
+// ivisible by 6, and you should print only the division by 6. If a number is divisible
+// by 2 it is sometimes also divisible by 10, and you should print the division by 10.
+// If the number is not divisible by any given number, print "Not divisible".
+// Otherwise, print "The number is divisible by {number}".
