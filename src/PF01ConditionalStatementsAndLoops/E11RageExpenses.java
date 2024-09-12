9f995dde6cb6ae14a2fd28a577b3d99871b2baf4
Todor Proykov <tproykov@gmail.com>
2024-09-12 07:46:00 +0300

    rage expenses

--- src/PF01ConditionalStatementsAndLoops/E11RageExpenses.java
+++ src/PF01ConditionalStatementsAndLoops/E11RageExpenses.java
@@ -8,8 +8,43 @@ public class E11RageExpenses {
 
         Scanner scanner = new Scanner(System.in);
 
+        int lostGamesCount = Integer.parseInt(scanner.nextLine());
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
 
+        int headsetsToReplace = lostGamesCount / 2;
+        int mousesToReplace = lostGamesCount / 3;
+        int keyboardsToReplace = lostGamesCount / 6;
+        int displaysToReplace = lostGamesCount / 12;
 
+        double rageExpenses =
+                headsetPrice * headsetsToReplace
+                        + mousePrice * mousesToReplace
+                        + keyboardPrice * keyboardsToReplace
+                        + displayPrice * displaysToReplace;
+
+        System.out.printf("Rage expenses: %.2flv.", rageExpenses);
     }
-
 }
+
+// As a MOBA challenger player, Peter has the bad habit of trashing his PC when he loses
+// a game and rage quits. His gaming setup consists of a headset, mouse, keyboard, and
+// display. You will receive Peter's lost games count.
+//Every second lost game, Peter trashes his headset.
+//Every third lost game, Peter trashes his mouse.
+//When Peter trashes both his mouse and headset in the same lost game, he also trashes
+// his keyboard.
+//Every second time when he trashes his keyboard, he also trashes his display.
+//You will receive the price of each item in his gaming setup. Calculate his rage expenses
+// for renewing his gaming equipment.
+//Input / Constraints
+//•	On the first input line - lost games count – integer in the range [0, 1000].
+//•	On the second line – headset price - the floating-point number in the range [0, 1000].
+//•	On the third line – mouse price - the floating-point number in the range [0, 1000].
+//•	On the fourth line – keyboard price - the floating-point number in the range [0, 1000].
+//•	On the fifth line – display price - the floating-point number in the range [0, 1000].
+//Output
+//•	As output you must print Peter's total expenses: "Rage expenses: {expenses} lv."
+//•	Allowed working time / memory: 100ms / 16MB.
