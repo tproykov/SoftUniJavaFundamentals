afdae975717c24cd0786702426d3ccfdee82c85c
tproykov <tproykov@gmail.com>
2024-09-22 19:31:59 +0100

    exercise 6

--- src/PF02DataTypesAndVariables/E06TriplesOfLetters.java
+++ src/PF02DataTypesAndVariables/E06TriplesOfLetters.java
@@ -1,4 +1,25 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E06TriplesOfLetters {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 97; i < 97 + n; i++) {
+            for (int j = 97; j < 97 + n; j++) {
+                for (int k = 97; k < 97 + n; k++) {
+                    char firstLetter = (char) i;
+                    char secondLetter = (char) j;
+                    char thirdLetter = (char) k;
+                    System.out.print("" + firstLetter + secondLetter + thirdLetter);
+                    System.out.println();
+                }
+            }
+        }
+    }
 }
