55f21b98395af93d44f2bf83e3a98cc952423cc7
tproykov <tproykov@gmail.com>
2024-09-22 19:17:06 +0100

    exercise 4

--- src/PF02DataTypesAndVariables/E04SumOfChars.java
+++ src/PF02DataTypesAndVariables/E04SumOfChars.java
@@ -1,4 +1,21 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E04SumOfChars {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        byte n = Byte.parseByte(scanner.nextLine());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+            char character = scanner.nextLine().charAt(0);
+            sum += character;
+        }
+        System.out.println("The sum equals: " + sum);
+    }
 }
