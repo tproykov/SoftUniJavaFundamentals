94cdbaa02b77dc97f8025bff9967a3172ae02dd9
tproykov <tproykov@gmail.com>
2024-09-22 19:40:35 +0100

    exercise 7

--- src/PF02DataTypesAndVariables/E07WaterOverflow.java
+++ src/PF02DataTypesAndVariables/E07WaterOverflow.java
@@ -1,4 +1,29 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E07WaterOverflow {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int capacity = 255;
+        int filled = 0;
+        for (int i = 1; i <= n; i++) {
+
+            int water = Integer.parseInt(scanner.nextLine());
+
+            if (water > capacity) {
+                System.out.println("Insufficient capacity!");
+            }
+            else {
+                filled += water;
+                capacity -= water;
+            }
+        }
+        System.out.println(filled);
+    }
 }
