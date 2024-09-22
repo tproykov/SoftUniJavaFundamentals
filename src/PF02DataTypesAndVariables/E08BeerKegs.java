0ec27b894738feab1dd39e9cc11e851abc01d9f4
tproykov <tproykov@gmail.com>
2024-09-22 19:49:27 +0100

    exercise 8

--- src/PF02DataTypesAndVariables/E08BeerKegs.java
+++ src/PF02DataTypesAndVariables/E08BeerKegs.java
@@ -1,4 +1,31 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E08BeerKegs {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        double maxVolume = 0;
+        String maxModel = "";
+        for (int i = 1; i <= n; i++) {
+
+            String model = scanner.nextLine();
+            double radius = Double.parseDouble(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+
+            double volume = Math.PI * Math.pow(radius, 2) * height;
+
+            if (maxVolume < volume) {
+                maxVolume = volume;
+                maxModel = model;
+            }
+        }
+        System.out.println(maxModel);
+    }
 }
