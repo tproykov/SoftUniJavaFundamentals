ce5b7011064477f7f9ec3537be3f490c407b122f
tproykov <tproykov@gmail.com>
2024-09-22 21:04:11 +0100

    exercise 11

--- src/PF02DataTypesAndVariables/E11Snowballs.java
+++ src/PF02DataTypesAndVariables/E11Snowballs.java
@@ -1,4 +1,34 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E11Snowballs {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberSnowballs = Integer.parseInt(scanner.nextLine());
+
+        int maxSnowballSnow = 0;
+        int maxSnowballTime = 0;
+        int maxSnowballQuality = 0;
+        long maxSnowballValue = 0;
+        for (int i = 0; i < numberSnowballs; i++) {
+
+            int snowballSnow = Integer.parseInt(scanner.nextLine());
+            int snowballTime = Integer.parseInt(scanner.nextLine());
+            int snowballQuality = Integer.parseInt(scanner.nextLine());
+            long snowballValue = (long) Math.pow((1.0 * snowballSnow / snowballTime), snowballQuality);
+
+            if (snowballValue > maxSnowballValue) {
+                maxSnowballValue = snowballValue;
+                maxSnowballTime = snowballTime;
+                maxSnowballQuality = snowballQuality;
+                maxSnowballSnow = snowballSnow;
+            }
+        }
+        System.out.printf("%d : %d = %d (%d)", maxSnowballSnow, maxSnowballTime, maxSnowballValue,
+                maxSnowballQuality);
+    }
 }
