7ed5d5e21e828cc0a788e8d6e1e6ec47f7a83bac
tproykov <tproykov@gmail.com>
2024-09-22 19:10:45 +0100

    exercise 3

--- src/PF02DataTypesAndVariables/E03Elevator.java
+++ src/PF02DataTypesAndVariables/E03Elevator.java
@@ -1,4 +1,23 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E03Elevator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int capacity = Integer.parseInt(scanner.nextLine());
+
+        int courses = 0;
+        if (n % capacity == 0) {
+            courses = n / capacity;
+        }
+        else {
+            courses = n / capacity + 1;
+        }
+        System.out.println(courses);
+    }
 }
