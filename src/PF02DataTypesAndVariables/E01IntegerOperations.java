c0e69b461c9e5fe3bcd5d5658599f77644d082f9
tproykov <tproykov@gmail.com>
2024-09-22 18:56:06 +0100

    exercise 1

--- src/PF02DataTypesAndVariables/E01IntegerOperations.java
+++ src/PF02DataTypesAndVariables/E01IntegerOperations.java
@@ -1,4 +1,18 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E01IntegerOperations {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+        int n3 = Integer.parseInt(scanner.nextLine());
+        int n4 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(((n1 + n2) / n3) * n4);
+    }
 }
