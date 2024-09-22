326262bf778e51e071d0183b7cc7723d232b4364
tproykov <tproykov@gmail.com>
2024-09-22 19:20:48 +0100

    exercise 5

--- src/PF02DataTypesAndVariables/E05PrintPartsOfASCII.java
+++ src/PF02DataTypesAndVariables/E05PrintPartsOfASCII.java
@@ -1,4 +1,21 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E05PrintPartsOfASCII {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int firstIndex = Integer.parseInt(scanner.nextLine());
+        int lastIndex = Integer.parseInt(scanner.nextLine());
+
+        for (int i = firstIndex; i <= lastIndex; i++) {
+
+            char character = (char) i;
+
+            System.out.print(character + " ");
+        }
+    }
 }
