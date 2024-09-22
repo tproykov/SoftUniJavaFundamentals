ae4056611bc8fa2a4a3eb6bf0da7b407c58598ef
tproykov <tproykov@gmail.com>
2024-09-22 20:38:07 +0100

    exercise 10

--- src/PF02DataTypesAndVariables/E10PokeMon.java
+++ src/PF02DataTypesAndVariables/E10PokeMon.java
@@ -1,4 +1,34 @@
 package PF02DataTypesAndVariables;
 
+import java.util.Scanner;
+
 public class E10PokeMon {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int initialPokePower = Integer.parseInt(scanner.nextLine());
+        int distance = Integer.parseInt(scanner.nextLine());
+        int exhaustionFactor = Integer.parseInt(scanner.nextLine());
+
+        int currentPokePower = initialPokePower;
+        int countPokes = 0;
+        while (true) {
+
+            if (currentPokePower == initialPokePower * 0.5 && exhaustionFactor != 0) {
+                currentPokePower = currentPokePower / exhaustionFactor;
+            }
+
+            if (currentPokePower < distance) {
+                break;
+            }
+            else {
+                currentPokePower -= distance;
+                countPokes++;
+            }
+        }
+        System.out.println(currentPokePower);
+        System.out.println(countPokes);
+    }
 }
