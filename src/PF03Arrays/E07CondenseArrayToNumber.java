e830d054cf032744c00254dc0d3a0ddac25fc272
tproykov <tproykov@gmail.com>
2024-09-23 21:33:22 +0100

    exercise 7

--- src/PF03Arrays/E07CondenseArrayToNumber.java
+++ src/PF03Arrays/E07CondenseArrayToNumber.java
@@ -1,4 +1,22 @@
 package PF03Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E07CondenseArrayToNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                array[j] = array[j] + array[j + 1];
+            }
+        }
+        System.out.println(array[0]);
+    }
 }
