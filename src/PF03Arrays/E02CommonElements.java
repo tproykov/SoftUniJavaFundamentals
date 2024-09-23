d0d615f1232c4cb9c1cf6400ba6782172d65721c
tproykov <tproykov@gmail.com>
2024-09-23 18:49:17 +0100

    exercise 2

--- src/PF03Arrays/E02CommonElements.java
+++ src/PF03Arrays/E02CommonElements.java
@@ -1,4 +1,22 @@
 package PF03Arrays;
 
+import java.util.Scanner;
+
 public class E02CommonElements {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] array1 = scanner.nextLine().split(" ");
+        String[] array2 = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array1.length; j++) {
+                if (array2[i].equals(array1[j])) {
+                    System.out.print(array2[i] + " ");
+                }
+            }
+        }
+    }
 }
