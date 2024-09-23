8a90c030484c5d9b668736f7d088beb4d594a728
tproykov <tproykov@gmail.com>
2024-09-23 18:54:57 +0100

    exercise 3

--- src/PF03Arrays/E03ZigZagArrays.java
+++ src/PF03Arrays/E03ZigZagArrays.java
@@ -1,4 +1,35 @@
 package PF03Arrays;
 
+import java.util.Scanner;
+
 public class E03ZigZagArrays {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] array1 = new int[n];
+        int[] array2 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            if (i % 2 == 0) {
+                array1[i] = scanner.nextInt();
+                array2[i] = scanner.nextInt();
+            }
+            else {
+                array2[i] = scanner.nextInt();
+                array1[i] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(array1[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(array2[i] + " ");
+        }
+    }
 }
