78c5914d79a6b6d771f74d6aac8d5c7d8bdf80f9
tproykov <tproykov@gmail.com>
2024-09-23 19:05:39 +0100

    exercise 4

--- src/PF03Arrays/E04ArrayRotation.java
+++ src/PF03Arrays/E04ArrayRotation.java
@@ -1,4 +1,30 @@
 package PF03Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E04ArrayRotation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt)
+                .toArray();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            int temp = array[0];
+            for (int j = 1; j < array.length; j++) {
+                array[j - 1] = array[j];
+            }
+            array[array.length - 1] = temp;
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
