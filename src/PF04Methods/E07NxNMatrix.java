8869c6e8776c5123ee79849ebb24f36f08b92420
tproykov <tproykov@gmail.com>
2024-09-25 22:01:18 +0100

    exercise 7

--- src/PF04Methods/E07NxNMatrix.java
+++ src/PF04Methods/E07NxNMatrix.java
@@ -1,4 +1,26 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class E07NxNMatrix {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        nxnMatrix(n);
+    }
+    private static void nxnMatrix(int n) {
+        // int[][] matrix = new int[n][n];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+// Write a method that receives a single integer n and prints an nxn matrix with that number.
