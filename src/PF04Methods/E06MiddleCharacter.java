ccb26e96b7c4f579077daaf8bd91126433558425
tproykov <tproykov@gmail.com>
2024-09-25 21:57:17 +0100

    exercise 6

--- src/PF04Methods/E06MiddleCharacter.java
+++ src/PF04Methods/E06MiddleCharacter.java
@@ -1,4 +1,27 @@
 package PF04Methods;
 
+import java.util.Scanner;
+
 public class E06MiddleCharacter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+
+        printMiddleCharacter(string);
+    }
+    private static void printMiddleCharacter(String string) {
+        if (string.length() % 2 != 0) {
+            System.out.println(string.charAt(string.length() / 2));
+        }
+        else {
+            System.out.print(string.charAt(string.length() / 2 - 1));
+            System.out.print(string.charAt(string.length() / 2));
+        }
+    }
 }
+
+// You will receive a single string. Write a method that prints the middle character. If the length of
+// the string is even, there are two middle characters.
