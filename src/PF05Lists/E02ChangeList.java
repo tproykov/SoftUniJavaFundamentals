65a3f3c1ea24ecfab4577d5ebe252a72835a80e1
tproykov <tproykov@gmail.com>
2024-09-26 19:36:49 +0100

    exercise 4

--- src/PF05Lists/E02ChangeList.java
+++ src/PF05Lists/E02ChangeList.java
@@ -41,3 +41,10 @@ public class E02ChangeList {
         }
     }
 }
+
+// Write a program that reads a list of integers from the console and receives commands which manipulate
+// the list. Your program may receive the following commands:
+//•	Delete {element} - delete all elements in the array which are equal to the given element
+//•	Insert {element} {position} - insert element at the given position
+//You should stop the program when you receive the command "end". Print all numbers in the array,
+// separated with a single whitespace.
--- src/PF05Lists/E04ListOperations.java
+++ src/PF05Lists/E04ListOperations.java
@@ -1,4 +1,83 @@
 package PF05Lists;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class E04ListOperations {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> list = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("End")) {
+
+            String[] tokens = command.split("\\s+");
+            String commandType = tokens[0];
+            int number = 0;
+            int index = 0;
+            int count = 0;
+            switch (commandType) {
+                case "Add" -> {
+                    number = Integer.parseInt(tokens[1]);
+                    list.add(number);
+                }
+                case "Insert" -> {
+                    number = Integer.parseInt(tokens[1]);
+                    index = Integer.parseInt(tokens[2]);
+                    if (index < 0 || index >= list.size()) {
+                        System.out.println("Invalid index");
+                        command = scanner.nextLine();
+                        continue;
+                    }
+                    list.add(index, number);
+                }
+                case "Remove" -> {
+                    index = Integer.parseInt(tokens[1]);
+                    if (index < 0 || index >= list.size()) {
+                        System.out.println("Invalid index");
+                        command = scanner.nextLine();
+                        continue;
+                    }
+                    list.remove(index);
+                }
+                case "Shift" -> {
+                    count = Integer.parseInt(tokens[2]);
+                    if (tokens[1].equals("left")) {
+                        for (int i = 0; i < count; i++) {
+                            number = list.getFirst();
+                            list.add(number);
+                            list.removeFirst();
+                        }
+                    }
+                    else if (tokens[1].equals("right")) {
+                        for (int i = 0; i < count; i++) {
+                            number = list.getLast();
+                            list.addFirst(number);
+                            list.removeLast();
+                        }
+                    }
+                }
+            }
+            command = scanner.nextLine();
+        }
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+    }
 }
+
+// You will be given numbers (a list of integers) on the first input line. Until you receive "End"
+// you will be given operations you must apply on the list. The possible commands are:
+//•	Add {number} - add number at the end
+//•	Insert {number} {index} - insert number at given index
+//•	Remove {index} - remove that index
+//•	Shift left {count} - first number becomes last 'count' times
+//•	Shift right {count} - last number becomes first 'count' times
+//Note: The index given may be outside the array's bounds. In that case, print "Invalid index".
