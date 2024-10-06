4484fa1c5ef7df39019121414478aaa280d5f8a0
tproykov <tproykov@gmail.com>
2024-10-06 11:20:57 +0100

    Update E13MemoryGame.java

--- src/PFE1MidTremExam/E13MemoryGame.java
+++ src/PFE1MidTremExam/E13MemoryGame.java
@@ -1,4 +1,17 @@
 package PFE1MidTremExam;
 
+import java.util.Scanner;
+
 public class E13MemoryGame {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+    }
+
 }
