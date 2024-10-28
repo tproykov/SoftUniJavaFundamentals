a339a9bbc0765bdeb262e2b818a377aa839d039e
tproykov <tproykov@gmail.com>
2024-10-28 19:58:00 +0000

    legendary farming

--- src/PF07AssociativeArrays/E07LegendaryFarming.java
+++ src/PF07AssociativeArrays/E07LegendaryFarming.java
@@ -1,6 +1,7 @@
 package PF07AssociativeArrays;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -12,27 +13,62 @@ public class E07LegendaryFarming {
 
         String[] input = scanner.nextLine().split("\\s+");
 
-        Map<String, Integer> keyMaterials = new HashMap<>();
+        Map<String, Integer> keyMaterials = new LinkedHashMap<>();
+        // Initialize key materials with 0
+        keyMaterials.put("shards", 0);
+        keyMaterials.put("fragments", 0);
+        keyMaterials.put("motes", 0);
 
-        for (int i = 1; i < input.length; i += 2) {
+        String winningMaterial = "";
+        String legendaryItem = "";
+        boolean legendaryItemObtained = false;
 
-            String material = input[i];
-            int quantity = Integer.parseInt(input[i - 1]);
+        while (true) {
 
-            if (!keyMaterials.containsKey(material)) {
-                keyMaterials.put(material, quantity);
+            for (int i = 1; i < input.length; i += 2) {
+
+                String material = input[i].toLowerCase();
+                int quantity = Integer.parseInt(input[i - 1]);
+
+                if (!keyMaterials.containsKey(material)) {
+                    keyMaterials.put(material, quantity);
+                } else {
+                    keyMaterials.put(material, keyMaterials.get(material) + quantity);
+                }
+
+                int totalQuantity = keyMaterials.get(material);
+                if (totalQuantity >= 250 && (material.equals("shards")
+                        || material.equals("fragments")
+                        || material.equals("motes"))) {
+                    legendaryItemObtained = true;
+                    winningMaterial = material;
+                    keyMaterials.put(winningMaterial, keyMaterials.get(winningMaterial) - 250);
+                    break;
+                }
             }
-            else {
-                keyMaterials.put(material, keyMaterials.get(material) + quantity);
+
+            if (legendaryItemObtained) {
+                switch (winningMaterial) {
+                    case "shards" -> legendaryItem = "Shadowmourne";
+                    case "fragments" -> legendaryItem = "Valanyr";
+                    case "motes" -> legendaryItem = "Dragonwrath";
+                }
+                System.out.println(legendaryItem + " obtained!");
+
+                System.out.println("shards: " + keyMaterials.get("shards"));
+                System.out.println("fragments: " + keyMaterials.get("fragments"));
+                System.out.println("motes: " + keyMaterials.get("motes"));
+
+                for (String currentMaterial : keyMaterials.keySet()) {
+                    if (!currentMaterial.equals("shards") && !currentMaterial.equals("fragments")
+                            && !currentMaterial.equals("motes")) {
+                        System.out.println(currentMaterial + ": "
+                                + keyMaterials.get(currentMaterial));
+                    }
+                }
+                return;
             }
-
-
-
+            input = scanner.nextLine().split("\\s+");
         }
-
-
-
-
     }
-
 }
