b91a2cabd1919beb9a24c0e689c41518f053ca79
tproykov <tproykov@gmail.com>
2024-11-02 23:00:11 +0000

    Update L04TextFilter.java

--- src/PF08TextProcessing/L04TextFilter.java
+++ src/PF08TextProcessing/L04TextFilter.java
@@ -1,4 +1,15 @@
 package PF08TextProcessing;
 
+import java.util.Scanner;
+
 public class L04TextFilter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
