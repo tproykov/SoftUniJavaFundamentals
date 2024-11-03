9b9b7f6860c38f6354f4efe44d89355dec97b60b
tproykov <tproykov@gmail.com>
2024-11-03 06:56:21 +0000

    update

--- src/PF08TextProcessing/E06ReplaceRepeatingChars.java
+++ src/PF08TextProcessing/E06ReplaceRepeatingChars.java
@@ -1,4 +1,26 @@
 package PF08TextProcessing;
 
+import java.util.Scanner;
+
 public class E06ReplaceRepeatingChars {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        StringBuilder output = new StringBuilder();
+        char lastChar = ' ';
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char character = input.charAt(i);
+            if (character != lastChar) {
+                output.append(character);
+            }
+            lastChar = character;
+        }
+        System.out.println(output);
+    }
 }
