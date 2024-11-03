b2d6b41e1008b14dc42be7cf8a04fa264d86636b
tproykov <tproykov@gmail.com>
2024-11-03 19:56:06 +0000

    update

--- src/PF08TextProcessing/M01ExtractPersonInformation.java
+++ src/PF08TextProcessing/M01ExtractPersonInformation.java
@@ -1,4 +1,29 @@
 package PF08TextProcessing;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class M01ExtractPersonInformation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String input = scanner.nextLine();
+
+            String[] nameParts1 = input.split("@");
+            String[] nameParts2 = nameParts1[1].split("\\|");
+            String name = nameParts2[0];
+
+            String[] ageParts1 = input.split("#");
+            String[] ageParts2 = ageParts1[1].split("\\*");
+            int age = Integer.parseInt(ageParts2[0]);
+
+            System.out.println(name + " is " + age + " years old.");
+        }
+    }
 }
--- src/PF08TextProcessing/M02AsciiSumator.java
+++ src/PF08TextProcessing/M02AsciiSumator.java
@@ -1,4 +1,39 @@
 package PF08TextProcessing;
 
+import java.util.Scanner;
+
 public class M02AsciiSumator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        char character1 = scanner.next().charAt(0);
+        char character2 = scanner.next().charAt(0);
+        String string = scanner.nextLine();
+
+        int index1 = 0;
+        int index2 = 0;
+        for (int i = 0; i < string.length(); i++) {
+
+            if (string.charAt(i) == character1) {
+                index1 = i;
+            }
+            if (string.charAt(i) == character2) {
+                index2 = i;
+            }
+        }
+
+        int sum = 0;
+        if (index1 < index2) {
+
+            
+
+
+
+        }
+
+
+    }
+
 }
