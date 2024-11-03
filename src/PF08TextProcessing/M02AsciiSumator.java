5870cac430592560e62f221f98eb8c6631946a4e
tproykov <tproykov@gmail.com>
2024-11-03 20:29:49 +0000

    update

--- src/PF08TextProcessing/M02AsciiSumator.java
+++ src/PF08TextProcessing/M02AsciiSumator.java
@@ -6,34 +6,40 @@ public class M02AsciiSumator {
 
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
 
         char character1 = scanner.next().charAt(0);
         char character2 = scanner.next().charAt(0);
+        scanner.nextLine();
         String string = scanner.nextLine();
 
-        int index1 = 0;
-        int index2 = 0;
-        for (int i = 0; i < string.length(); i++) {
-
-            if (string.charAt(i) == character1) {
-                index1 = i;
-            }
-            if (string.charAt(i) == character2) {
-                index2 = i;
-            }
-        }
-
         int sum = 0;
-        if (index1 < index2) {
 
-            
+        if (character1 < character2) {
 
+            for (int i = 0; i < string.length(); i++) {
 
+                char character = string.charAt(i);
 
+                if (character > character1 && character < character2) {
+
+                    sum += character;
+                }
+            }
         }
+        else {
 
+            for (int i = 0; i < string.length(); i++) {
 
+                char character = string.charAt(i);
+
+                if (character > character2 && character < character1) {
+
+                    sum += character;
+                }
+            }
+        }
+        System.out.println(sum);
     }
-
 }
