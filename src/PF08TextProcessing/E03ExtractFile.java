e3e707228fc3bf817b844ac853d24e7caef12895
tproykov <tproykov@gmail.com>
2024-11-03 05:39:16 +0000

    updated

--- src/PF08TextProcessing/E03ExtractFile.java
+++ src/PF08TextProcessing/E03ExtractFile.java
@@ -1,4 +1,23 @@
 package PF08TextProcessing;
 
+import java.util.Scanner;
+
 public class E03ExtractFile {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] urlParts = scanner.nextLine().split("\\\\");
+
+        String file = urlParts[urlParts.length - 1];
+
+        String[] fileParts = file.split("\\.");
+
+        String fileName = fileParts[0];
+        String fileExtension = fileParts[fileParts.length - 1];
+
+        System.out.println("File name: " + fileName);
+        System.out.println("File extension: " + fileExtension);
+    }
 }
--- src/PF08TextProcessing/E04CaesarCipher.java
+++ src/PF08TextProcessing/E04CaesarCipher.java
@@ -1,4 +1,15 @@
 package PF08TextProcessing;
 
+import java.util.Scanner;
+
 public class E04CaesarCipher {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
 }
