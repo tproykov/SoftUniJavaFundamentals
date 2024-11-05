90f38b4425790d4303ebdd6d86ceefe7e1b9e80a
tproykov <tproykov@gmail.com>
2024-11-05 23:07:59 +0000

    update foundations

--- src/PF08TextProcessing/M05HTML.java
+++ src/PF08TextProcessing/M05HTML.java
@@ -1,4 +1,29 @@
 package PF08TextProcessing;
 
+import java.util.Scanner;
+
 public class M05HTML {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String title = scanner.nextLine();
+        String content = scanner.nextLine();
+
+        System.out.println("<h1>");
+        System.out.println("    " + title);
+        System.out.println("</h1>");
+        System.out.println("<article>");
+        System.out.println("    " + content);
+        System.out.println("</article>");
+
+        String comment;
+        while (!(comment = scanner.nextLine()).equals("end of comments")) {
+
+            System.out.println("<div>");
+            System.out.println("    " + comment);
+            System.out.println("</div>");
+        }
+    }
 }
