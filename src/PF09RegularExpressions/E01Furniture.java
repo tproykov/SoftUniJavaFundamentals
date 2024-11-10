5338c582e49d1665db376fb689d066931b6afcfa
tproykov <tproykov@gmail.com>
2024-11-10 21:14:08 +0000

    Update E01Furniture.java

--- src/PF09RegularExpressions/E01Furniture.java
+++ src/PF09RegularExpressions/E01Furniture.java
@@ -1,5 +1,7 @@
 package PF09RegularExpressions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class E01Furniture {
@@ -7,6 +9,19 @@ public class E01Furniture {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        List<String> furniture = new ArrayList<>();
+        double totalCost = 0;
+
+        String regex = "";
+
+        String input;
+        while (!(input = scanner.nextLine()).equals("Purchase")) {
+
+
+
+
+        }
 
 
 
