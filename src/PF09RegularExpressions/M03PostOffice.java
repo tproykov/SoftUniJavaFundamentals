78cca9ca9af91566d69cdcebddb1e97cd3670578
tproykov <tproykov@gmail.com>
2024-11-11 13:51:52 +0000

    Update M03PostOffice.java

--- src/PF09RegularExpressions/M03PostOffice.java
+++ src/PF09RegularExpressions/M03PostOffice.java
@@ -1,4 +1,16 @@
 package PF09RegularExpressions;
 
+import java.util.Scanner;
+
 public class M03PostOffice {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
+
 }
