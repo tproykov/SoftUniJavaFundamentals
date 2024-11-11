9e3b7c15409363e8754e8e2e9b41db85ccae458e
tproykov <tproykov@gmail.com>
2024-11-11 10:50:06 +0000

    update regex

--- src/PF09RegularExpressions/E05ExtractEmails.java
+++ src/PF09RegularExpressions/E05ExtractEmails.java
@@ -1,6 +1,8 @@
 package PF09RegularExpressions;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class E05ExtractEmails {
 
@@ -8,8 +10,16 @@ public class E05ExtractEmails {
 
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
+        String regex = "[A-Za-z0-9]+[.\\-_]*[A-Za-z0-9]*@[a-z]+[\\-]*[a-z]*.[a-z]+(\\.[a-z]+)*";
 
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+
+            System.out.println(matcher.group());
+        }
     }
-
 }
