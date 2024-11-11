bd72a333b6220b76665926fe1aae92e46e8b9386
tproykov <tproykov@gmail.com>
2024-11-11 17:55:41 +0000

    update regex

--- src/PF09RegularExpressions/M03PostOffice.java
+++ src/PF09RegularExpressions/M03PostOffice.java
@@ -1,8 +1,6 @@
 package PF09RegularExpressions;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,10 +16,15 @@ public class M03PostOffice {
         String startLetterAndLength = inputParts[1];
         String wordsString = inputParts[2];
 
-        String regexCapitalLetters = "([#$%*&])[A-Z]+\\1";
+        String regexCapitalLetters = "([#$%*&])([A-Z]+)\\1";
         Pattern patternCapitalLetters = Pattern.compile(regexCapitalLetters);
         Matcher matcherCapitalLetters = patternCapitalLetters.matcher(capitalLettersString);
-        String capitalLetters = matcherCapitalLetters.group();
+        
+        String capitalLetters = "";
+        while (matcherCapitalLetters.find()) {
+            capitalLetters = matcherCapitalLetters.group(2);
+        }
+
         List<String> capitalLettersList = new ArrayList<>();
         for (int i = 0; i < capitalLetters.length(); i++) {
             capitalLettersList.add(capitalLetters.charAt(i) + "");
@@ -29,21 +32,39 @@ public class M03PostOffice {
 
         String regexStartLetterAndLength = "\\d{2}:\\d{2}";
         Pattern patternStartLetterAndLength = Pattern.compile(regexStartLetterAndLength);
-        Matcher matcherStartLetterAndLength = patternStartLetterAndLength.matcher(capitalLetters);
+        Matcher matcherStartLetterAndLength = patternStartLetterAndLength.matcher(startLetterAndLength);
         List<String> startLetterAndLengthList = new ArrayList<>();
         while (matcherStartLetterAndLength.find()) {
             startLetterAndLengthList.add(matcherStartLetterAndLength.group());
         }
-        for (int i = 0; i < capitalLettersList.size(); i++) {
 
-            
+        Map<Character, Integer> capitalsStartAndLength = new LinkedHashMap<>();
+        for (String s : capitalLettersList) {
+            for (String string : startLetterAndLengthList) {
 
+                char character = s.charAt(0);
+                String[] startingLetters = string.split(":");
+                int startingLetter = Integer.parseInt(startingLetters[0]);
+                int wordLength = Integer.parseInt(startingLetters[1]);
+
+                if (character == startingLetter) {
+                    capitalsStartAndLength.put(character, wordLength);
+                }
+            }
         }
 
+        String[] words = wordsString.split("\\s+");
+        for (String word : words) {
 
+            char startingLetter = word.charAt(0);
 
+            for (Character capitalLetter : capitalsStartAndLength.keySet()) {
 
+                if (capitalLetter == startingLetter && (word.length() - 1) == capitalsStartAndLength.get(capitalLetter)) {
+
+                    System.out.println(word);
+                }
+            }
+        }
     }
-
-
 }
