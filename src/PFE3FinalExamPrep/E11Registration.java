100f1fd7458cd0f7589af311beb8f7c90ed192e2
tproykov <tproykov@gmail.com>
2024-11-12 20:22:49 +0000

    update

--- src/PFE3FinalExamPrep/E11Registration.java
+++ src/PFE3FinalExamPrep/E11Registration.java
@@ -20,13 +20,10 @@ public class E11Registration {
                 case "Letters" -> {
                     String letterCase = tokens[1];
                     switch (letterCase){
-                        case "Lower" -> {
-                            username = username.toLowerCase();
-                        }
-                        case "Upper" -> {
-                            username = username.toUpperCase();
-                        }
+                        case "Lower" -> username = username.toLowerCase();
+                        case "Upper" -> username = username.toUpperCase();
                     }
+                    System.out.println(username);
                 }
 
                 case "Reverse" -> {
@@ -42,14 +39,29 @@ public class E11Registration {
 
                 case "Substring" -> {
                     String substring = tokens[1];
+                    if (username.contains(substring)){
+                        username = username.replace(substring, "");
+                        System.out.println(username);
+                    }
+                    else {
+                        System.out.println("The username " + username + " doesn't contain " + substring + ".");
+                    }
                 }
 
                 case "Replace" -> {
-                    char replaceChar = tokens[1].charAt(0);
+                    String replaceChar = tokens[1];
+                    username = username.replace(replaceChar, "-");
+                    System.out.println(username);
                 }
 
                 case "IsValid" -> {
-                    char isValidChar = tokens[1].charAt(0);
+                    String isValidChar = tokens[1];
+                    if (username.contains(isValidChar)){
+                        System.out.println("Valid username.");
+                    }
+                    else {
+                        System.out.println(isValidChar + " must be contained in your username.");
+                    }
                 }
             }
         }
