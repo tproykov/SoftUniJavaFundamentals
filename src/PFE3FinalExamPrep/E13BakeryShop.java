c2c27305a0b6912b23ba30b10eea3cc5051b16d7
tproykov <tproykov@gmail.com>
2024-11-13 21:54:44 +0000

    Update E13BakeryShop.java

--- src/PFE3FinalExamPrep/E13BakeryShop.java
+++ src/PFE3FinalExamPrep/E13BakeryShop.java
@@ -1,5 +1,7 @@
 package PFE3FinalExamPrep;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class E13BakeryShop {
@@ -8,8 +10,47 @@ public class E13BakeryShop {
 
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, Integer> foodQuantity = new HashMap<>();
+        int foodQuantitySold = 0;
 
+        String command;
+        while (!(command = scanner.nextLine()).equals("Complete")) {
 
+            String[] tokens = command.split("\\s+");
+            String commandType = tokens[0];
+            int quantity = Integer.parseInt(tokens[1]);
+            String productName = tokens[2];
+
+            switch (commandType) {
+
+                case "Receive" -> {
+                    if (quantity > 0) {
+                        if (!foodQuantity.containsKey(productName)) {
+                            foodQuantity.put(productName, quantity);
+                        }
+                        else {
+                            foodQuantity.put(productName, foodQuantity.get(productName) + quantity);
+                        }
+                    }
+                }
+
+                case "Sell" -> {
+                    if (!foodQuantity.containsKey(productName)) {
+                        System.out.println("You do not have any " + productName + ".");
+                    }
+                    else {
+                        if (foodQuantity.get(productName) <= quantity) {
+                            foodQuantitySold += foodQuantity.get(productName);
+                            foodQuantity.remove(productName);
+                            System.out.println("There aren't enough " + productName + ". You sold the last "
+                                    + foodQuantity.get(productName) + " of them.");
+                        }
+                        else {
+                            foodQuantity.put(productName, foodQuantity.get(productName) - quantity);
+                        }
+                    }
+                }
+            }
+        }
     }
-
 }
