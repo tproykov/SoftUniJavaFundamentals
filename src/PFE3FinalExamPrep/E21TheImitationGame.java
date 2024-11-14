22d03e655d224b8cb6d934e2de6326e7a968dd48
tproykov <tproykov@gmail.com>
2024-11-14 20:55:20 +0000

    update

--- src/PFE3FinalExamPrep/E21TheImitationGame.java
+++ src/PFE3FinalExamPrep/E21TheImitationGame.java
@@ -12,31 +12,42 @@ public class E21TheImitationGame {
 
         String command;
 
-        while(!(command = scanner.nextLine()).equals("Decode")){
+        while (!(command = scanner.nextLine()).equals("Decode")) {
 
-            String[] tokens = input.split("\\|");
+            String[] tokens = command.split("\\|");
             String commandType = tokens[0];
 
-            switch (commandType){
+            switch (commandType) {
+
                 case "Move" -> {
                     int numberOfLetters = Integer.parseInt(tokens[1]);
-
+                    if (numberOfLetters <= input.length()) {
+                        String substringToMove = input.substring(0, numberOfLetters);
+                        String remainingPart = input.substring(numberOfLetters);
+                        input = remainingPart + substringToMove;
+                    }
                 }
-                case "Insert" -> {}
-                case "ChangeAll" -> {}
+
+                case "Insert" -> {
+                    int index = Integer.parseInt(tokens[1]);
+                    String value = tokens[2];
+                    if (index >= 0 && index <= input.length()) {
+                        StringBuilder stringBuilder = new StringBuilder(input);
+                        stringBuilder.insert(index, value);
+                        input = stringBuilder.toString();
+                    }
+                }
+
+                case "ChangeAll" -> {
+                    String substringToChange = tokens[1];
+                    String replacement = tokens[2];
+                    if (input.contains(substringToChange)) {
+                        input = input.replaceAll(substringToChange, replacement);
+                    }
+                }
             }
-
-
-
-
-
-
-
-
         }
 
-
-
+        System.out.println("The decrypted message is: " + input);
     }
-
 }
