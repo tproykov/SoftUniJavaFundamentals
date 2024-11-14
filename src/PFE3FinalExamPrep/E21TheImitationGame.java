85abb6c480560b1f719836afc193eb940db7c5c6
tproykov <tproykov@gmail.com>
2024-11-14 20:58:50 +0000

    Update E21TheImitationGame.java

--- src/PFE3FinalExamPrep/E21TheImitationGame.java
+++ src/PFE3FinalExamPrep/E21TheImitationGame.java
@@ -3,47 +3,37 @@ package PFE3FinalExamPrep;
 import java.util.Scanner;
 
 public class E21TheImitationGame {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         String input = scanner.nextLine();
-
         String command;
 
         while (!(command = scanner.nextLine()).equals("Decode")) {
-
             String[] tokens = command.split("\\|");
             String commandType = tokens[0];
 
             switch (commandType) {
-
                 case "Move" -> {
                     int numberOfLetters = Integer.parseInt(tokens[1]);
-                    if (numberOfLetters <= input.length()) {
+                    if (numberOfLetters > 0 && numberOfLetters <= input.length()) {
                         String substringToMove = input.substring(0, numberOfLetters);
                         String remainingPart = input.substring(numberOfLetters);
                         input = remainingPart + substringToMove;
                     }
                 }
-
                 case "Insert" -> {
                     int index = Integer.parseInt(tokens[1]);
                     String value = tokens[2];
-                    if (index >= 0 && index <= input.length()) {
+                    if (index >= 0 && index <= input.length() && value != null) {
                         StringBuilder stringBuilder = new StringBuilder(input);
                         stringBuilder.insert(index, value);
                         input = stringBuilder.toString();
                     }
                 }
-
                 case "ChangeAll" -> {
                     String substringToChange = tokens[1];
                     String replacement = tokens[2];
-                    if (input.contains(substringToChange)) {
-                        input = input.replaceAll(substringToChange, replacement);
-                    }
+                    input = input.replace(substringToChange, replacement);
                 }
             }
         }
