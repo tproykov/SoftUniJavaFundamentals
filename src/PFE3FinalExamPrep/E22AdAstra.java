728044ec9ae1c73e5833e0bf77964239099825de
tproykov <tproykov@gmail.com>
2024-11-14 21:20:32 +0000

    update

--- src/PFE3FinalExamPrep/E22AdAstra.java
+++ src/PFE3FinalExamPrep/E22AdAstra.java
@@ -1,12 +1,29 @@
 package PFE3FinalExamPrep;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class E22AdAstra {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "([\\|#])(?<itemName>[A-Za-z\\s]+)\\1(?<expirationDate>\\d{2}\\/\\d{2}\\/\\d{2})\\1(?<calories>\\d+)\\1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        int totalCalories = 0;
+        while (matcher.find()) {
+            totalCalories += Integer.parseInt(matcher.group("calories"));
+        }
+
+        int daysWithFood = totalCalories / 2000;
+
+        System.out.println("You have food to last you for: " + daysWithFood + " days!");
 
 
 
